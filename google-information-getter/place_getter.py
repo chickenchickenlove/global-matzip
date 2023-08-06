5d844589b6b9f810222150ce6f26e49c31c69163
chickenchickenlove <ojt90902@naver.com>
2023-08-06 14:40:04 +0900

    implement feature google_information_getter

--- google-information-getter/place_getter.py
+++ google-information-getter/place_getter.py
@@ -2,7 +2,9 @@ import asyncio
 import logging
 import googlemaps
 import contextlib
+import time
 
+from file_writer import Writer
 from where_am_i import Coordinator
 
 
@@ -19,107 +21,81 @@ def get_gmaps_client(api_key):
 class FoodAggregator:
 
     def __init__(self,
+                 city_name: str,
                  coordinator: Coordinator,
                  diameter: float,
                  api_key: str,
                  find_type: str):
 
+        self._total_cnt = 0
+        self._city_name = city_name
         self._coordinator = coordinator
         self._api_key = api_key
         self._radius = diameter / 2
         self._find_type = find_type
 
+        # init directory for saving data.
+        self._writer = Writer(self.city_name())
+
+    def city_name(self) -> str:
+        return self._city_name
+
     # {'html_attributions': [], 'results': [], 'status': 'ZERO_RESULTS'}
     # {'html_attributions': [], 'results': [dict1, dict2, ...], 'status': 'OK'}
     def get_place_data(self,
-                       cur_location: tuple):
-        ret_dict = dict()
+                       cur_location: tuple) -> None:
+
+        # for logging.
+        logging.info(f'{cur_location = }')
+        print(f'{cur_location = }')
+
+        file_path = self._writer.create_new_file_path()
+
         next_page_token = -1
+        params = {
+            'location': cur_location,
+            'radius': self._radius,
+            'language': 'ko',
+            'type': self._find_type,
+            'keyword': 'food'}
 
         with get_gmaps_client(self._api_key) as client:
             while next_page_token is not None:
+                self._total_cnt += 1
 
-                params = {
-                    'location': cur_location,
-                    'radius': self._radius,
-                    'language': 'ko',
-                    'type': self._find_type,
-                    'keyword': '맛집'}
-
-                if next_page_token != -1:
-                    params.update(page_token=next_page_token)
-
-                result_dict = client.places_nearby(**params)
+                if next_page_token is None or next_page_token == -1:
+                    result_dict = client.places_nearby(**params)
+                else:
+                    print('sleep 5, because we have next token. next token has delay for applying to server.')
+                    time.sleep(5)
+                    try:
+                        result_dict = client.places_nearby(page_token=next_page_token)
+                    except googlemaps.exceptions.ApiError as e:
+                        print('Invalid API Request occur. we break this loop to escape big charge from google.')
+                        logging.warning('Invalid API Request occur. we break this loop to escape big charge from google.')
+                        break
 
                 if result_dict['status'] != 'OK':
                     break
 
                 response_list = result_dict['results']
-                self.__transfer_dict(ret_dict, response_list)
+                self._writer.write(response_list, file_path)
 
                 next_page_token = None if 'next_page_token' not in result_dict else result_dict['next_page_token']
 
-        return ret_dict
+        print(f'now count = {self._total_cnt}')
 
-    def __transfer_dict(self, ret_dict: dict, response_list: list):
+    async def get_place(self, is_async) -> None:
 
-        '''
-        name, rating, vicinity
-        key = name / vicinity
-        value = (name, rating, vicinity)
-        '''
+        if not is_async:
+            [self.get_place_data(cur_location.get_position_with_tuple())
+             for cur_location in self._coordinator]
 
-        for response_dict in response_list:
-            name = response_dict['name']
-            vicinity = response_dict['vicinity']
-            rating = 0 if 'rating' not in response_dict else response_dict['rating']
-
-            key = name + '/' + vicinity
-            value = (name, vicinity, rating)
-
-            ret_dict[key] = value
-
-    async def get_place(self):
-        loop = asyncio.get_running_loop()
-        tasks = [loop.run_in_executor(None,
-                                      self.get_place_data,
-                                      cur_location.get_position_with_tuple())
-                 for cur_location in self._coordinator]
-
-        result_dict = dict()
-        ret_dicts = [await t for t in tasks]
-        for each_result_dict in ret_dicts:
-            result_dict.update(each_result_dict)
-
-        return result_dict
-
-
-
-
-
-# y1 = 21.04378
-# x1 = 105.81020
-# y2 = 20.98680
-# x2 = 105.86385
-# RADIUS = 10000
-
-
-# async def amain():
-#     coordinator = Coordinator(y1, x1, y2, x2, RADIUS * 2)
-#
-#     aggregator = FoodAggregator(
-#         coordinator,
-#         RADIUS * 2,
-#         API_KEY,
-#         FIND_TYPE)
-#
-#     print(aggregator)
-#     my_list = await aggregator.get_place()
-#     print(my_list)
-#
-#
-# def main():
-#     asyncio.run(amain())
-#
-#
-# main()
+        else:
+            loop = asyncio.get_running_loop()
+            tasks = [loop.run_in_executor(None,
+                                          self.get_place_data,
+                                          cur_location.get_position_with_tuple())
+                     for cur_location in self._coordinator]
+            for t in tasks:
+                await t
