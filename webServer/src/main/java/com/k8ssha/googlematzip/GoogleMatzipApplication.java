666c710ce9c4dce787ca609b9aadcd31f3818a9a
chickenchickenlove <ojt90902@naver.com>
2023-08-06 15:11:02 +0900

    init web server

---
+++ webServer/src/main/java/com/k8ssha/googlematzip/GoogleMatzipApplication.java
@@ -0,0 +1,13 @@
+package com.k8ssha.googlematzip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleMatzipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleMatzipApplication.class, args);
+	}
+
+}
